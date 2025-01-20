85a02b55be9ff419d91927b2cc2057d1ba7eb90b
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-01-20 11:47:22 +0100

    complete mobile nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,31 @@ import logo from "../../assets/logo.png"
 import { HiOutlineShoppingCart } from "react-icons/hi2";
 import { IoMdMenu } from "react-icons/io";
 import { useState, useEffect } from "react";
+import { MdOutlineClose } from "react-icons/md";
 
 const todayDate = new Date().toString().slice(0, 15);
-const topText = ["Premium products at affordable prices", `today is ${todayDate}`]
-    
+const topText = ["Premium products at affordable prices", `today is ${todayDate}`];
 
 const Header = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [toggler, setToggler] = useState(false);
+
+    /* const handleToggle = () => {
+        setToggler(!toggler);
+    }
+ */
+    const handleToggle = () => {
+        if (toggler) {
+            // Start the remove animation when closing
+            document.querySelector(".mobile_dropdown").classList.add("remove-roll");
+            setTimeout(() => {
+                setToggler(!toggler); // After animation finishes, hide the menu
+                document.querySelector(".mobile_dropdown").classList.remove("remove-roll");
+            }, 1000); // Delay should match the animation duration
+        } else {
+            setToggler(!toggler); // Show the menu and trigger the open animation
+        }
+    }
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -42,7 +60,24 @@ const Header = () => {
                     </div>
 
                     <div className="mobile">
-                        <IoMdMenu />
+                        <IoMdMenu className="toggler" id="toggler" onClick={handleToggle} />
+                        {toggler && (
+                            <div className="mobile_dropdown" id="mobile_dropdown">
+                                <div className="dropdown_inner">
+                                    <MdOutlineClose className="close-icon" onClick={handleToggle} />
+
+                                    <div className="dropdown-links">
+                                        <a href="#">home</a>
+                                        <a href="#" className="pdt">products</a>
+                                        <button>
+                                            <HiOutlineShoppingCart className="cart-icon" />
+                                            <span className="cart-value">0</span>
+                                        </button>
+                                        <a href="">sign in</a>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </nav>
