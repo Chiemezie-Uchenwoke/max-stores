de0c0af18428fb2046a1fd36ada67d695d6bc82a
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-02-07 17:09:42 +0100

    Update products and productCard styles

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -4,6 +4,20 @@ import product3 from "../assets/categories-items/6.jpg";
 import product4 from "../assets/categories-items/16.jpg";
 import product5 from "../assets/categories-items/17.jpg";
 import product6 from "../assets/categories-items/18.jpg";
+import product7 from "../assets/categories-items/15.jpg";
+import product8 from "../assets/categories-items/29.jpg";
+import product9 from "../assets/categories-items/30.jpg";
+import product10 from "../assets/categories-items/40.jpg";
+import product11 from "../assets/categories-items/41.jpg";
+import product12 from "../assets/categories-items/35.jpg";
+import product13 from "../assets/categories-items/44.jpg";
+import product14 from "../assets/categories-items/45.jpg";
+import product15 from "../assets/categories-items/47.jpg";
+import product16 from "../assets/categories-items/31.jpg";
+import product17 from "../assets/categories-items/38.jpg";
+import product18 from "../assets/categories-items/49.jpg";
+import product19 from "../assets/categories-items/50.jpg";
+import product20 from "../assets/categories-items/28.jpg";
 
 const products = [
     { 
@@ -44,6 +58,104 @@ const products = [
     { 
         id: 6, 
         src: product6, 
+        alt: "Smart Watch", 
+        name: "Smart Watch", 
+        price: 120.0 
+    },
+    { 
+        id: 7, 
+        src: product7, 
+        alt: "Smart Watch", 
+        name: "Smart Watch", 
+        price: 120.0 
+    },
+    { 
+        id: 8, 
+        src: product8, 
+        alt: "Smart Watch", 
+        name: "Smart Watch", 
+        price: 120.0 
+    },
+    { 
+        id: 9, 
+        src: product9, 
+        alt: "Smart Watch", 
+        name: "Smart Watch", 
+        price: 120.0 
+    },
+    { 
+        id: 10, 
+        src: product10, 
+        alt: "Smart Watch", 
+        name: "Smart Watch", 
+        price: 120.0 
+    },
+    { 
+        id: 11, 
+        src: product11, 
+        alt: "Smart Watch", 
+        name: "Smart Watch", 
+        price: 120.0 
+    },
+    { 
+        id: 12, 
+        src: product12, 
+        alt: "Smart Watch", 
+        name: "Smart Watch", 
+        price: 120.0 
+    },
+    { 
+        id: 13, 
+        src: product13, 
+        alt: "Smart Watch", 
+        name: "Smart Watch", 
+        price: 120.0 
+    },
+    { 
+        id: 14, 
+        src: product14, 
+        alt: "Smart Watch", 
+        name: "Smart Watch", 
+        price: 120.0 
+    },
+    { 
+        id: 15, 
+        src: product15, 
+        alt: "Smart Watch", 
+        name: "Smart Watch", 
+        price: 120.0 
+    },
+    { 
+        id: 16, 
+        src: product16, 
+        alt: "Smart Watch", 
+        name: "Smart Watch", 
+        price: 120.0 
+    },
+    { 
+        id: 17, 
+        src: product17, 
+        alt: "Smart Watch", 
+        name: "Smart Watch", 
+        price: 120.0 
+    },
+    { 
+        id: 18, 
+        src: product18, 
+        alt: "Smart Watch", 
+        name: "Smart Watch", 
+        price: 120.0 
+    },
+    { 
+        id: 19, 
+        src: product19, 
+        alt: "Smart Watch", 
+        name: "Smart Watch", 
+        price: 120.0 
+    },
+    { 
+        id: 20, 
+        src: product20, 
         alt: "Smart Watch", 
         name: "Smart Watch", 
         price: 120.0 
