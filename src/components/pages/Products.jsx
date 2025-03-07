047778830d4fb508a6977b56aa4fd0f27d9cbb65
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-03-07 13:02:43 +0100

    test product page

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,11 +1,11 @@
 import Header from "../Header/Header";
-import Intro from "../Intro/Intro";
+// import Intro from "../Intro/Intro";
 
 const Products = () => {
     return(
         <>
             <Header />
-            <Intro />
+            {/* <Intro /> */}
         </>
     )
 }
