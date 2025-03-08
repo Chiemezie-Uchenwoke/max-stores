79e7943f8cb606f73f8273f8c56c7b8755cc6967
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-03-08 16:28:33 +0100

    Add products to product page

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,11 +1,15 @@
 import Header from "../Header/Header";
 import Intro from "../Intro/Intro";
+import ProductGallery from "../ProductGallery/ProductGallery";
+import Footer from "../Footer/Footer";
 
 const Products = () => {
     return(
         <>
             <Header />
             <Intro />
+            <ProductGallery />
+            <Footer />
         </>
     )
 }
